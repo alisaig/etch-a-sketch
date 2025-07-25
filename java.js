b8a675cca0138ee07739a8652f311df0eb7e9b25
alisaig <alisa.ign20@gmail.com>
2025-07-26 01:53:40 +0200

    Create row of div squares

--- java.js
+++ java.js
@@ -1,3 +1,10 @@
 // Etch-a-sketch grid creation
+const gridDimensions = 16 ** 2;
 
-const div = document.createElement("div");
+const container = document.querySelector("#container");
+
+for (let i = 0; i < gridDimensions; i++) {
+    const div = document.createElement("div");
+    div.classList.add("box");
+    container.appendChild(div);
+}
