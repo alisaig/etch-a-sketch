a19e93e950980a18d6984969894e607494f63380
alisaig <alisa.ign20@gmail.com>
2025-07-26 04:20:05 +0200

    Allow buttons to change grid dimension through user input

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # etch-a-sketch
 A grid with squares that change color when you move the mouse over them
+
+Credits:
+Finger Paint font - Ralph Oliver du Carrois
+https://www.fontsquirrel.com/fonts/finger-paint
--- java.js
+++ java.js
@@ -1,19 +1,33 @@
 // Etch-a-sketch grid creation
-const gridDimensions = 16 ** 2;
+let gridDimensions = 16;
 
 const container = document.querySelector("#container");
+const sizeButton = document.querySelector("#size");
+const resetButton = document.querySelector("#reset");
 
-for (let i = 0; i < gridDimensions; i++) {
-    const div = document.createElement("div");
-    div.classList.add("box");
+function generateGrid() {
+    container.textContent = "";
+    for (let i = 0; i < gridDimensions ** 2; i++) {
+        const div = document.createElement("div");
+        div.classList.add("box");
+        
+        div.style.width = `calc((100% - 2px * (${gridDimensions} - 1)) / ${gridDimensions})`;
+        div.style.aspectRatio = "1 / 1";
 
-    div.addEventListener("mouseover", () => {
-        div.style.background = "red";
-    });
+        div.addEventListener("mouseover", () => {
+            div.style.background = "red";
+        });
 
-    container.appendChild(div);
+        container.appendChild(div);
+    }
 }
 
-// function colorIn() {
-//     div.style.background = "red";
-// }
+sizeButton.addEventListener("click", () => {
+    do {
+        gridDimensions = parseInt(prompt("Enter number (min 1, max 100) of squares for new grid:"));
+    } while (gridDimensions > 100 || gridDimensions < 1);
+
+    generateGrid();
+})
+
+generateGrid()
